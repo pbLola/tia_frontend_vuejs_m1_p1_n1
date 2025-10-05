e86067c020741a181aa48a2598a14470e0d63f61
pbLola <lohannecastro88@gmail.com>
2025-10-04 21:27:55 -0300

    feat(app): criar componente raiz App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,22 @@
-<template
+<template>
+  <div class="container app-root">
+    <cardProfile />
+  </div>
+</template>
+
+<script>
+import CardProfile from "./components/CardProfile.vue"
+
+export default {
+  name: "App",
+  components: { cardProfile },
+}
+</script>
+<style>
+.app-root {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+</style>
